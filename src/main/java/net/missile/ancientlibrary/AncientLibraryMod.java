b79f091308c469dd3285f7877227c9a02d3b4fc4
MissileMann <misael.escoto043@gmail.com>
2025-05-08 17:14:49 -0500

    apotheosis compat and creative tab change

--- src/main/java/net/missile/ancientlibrary/AncientLibraryMod.java
+++ src/main/java/net/missile/ancientlibrary/AncientLibraryMod.java
@@ -1,29 +1,19 @@
 package net.missile.ancientlibrary;
 
-import com.mojang.logging.LogUtils;
-import com.mojang.serialization.Codec;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import net.missile.ancientlibrary.setup.AncientLibraryInit;
-import net.missile.ancientlibrary.setup.AncientLibraryLootMod;
 import net.missile.ancientlibrary.setup.ModLootModifiers;
-import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(AncientLibraryMod.MODID)
 public class AncientLibraryMod
 {
     public static final String MODID = "ancientlibrary";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
 
     public AncientLibraryMod(FMLJavaModLoadingContext context)
@@ -47,7 +37,7 @@ public class AncientLibraryMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+        if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS)
             event.accept(AncientLibraryInit.ANCIENT_LIBRARY);
     }
 }
